adb0a1ea4c2e75eb6f70a51312c8a91efad699e8
Ojaswimakode07 <162621630+Ojaswimakode07@users.noreply.github.com>
2024-03-11 20:47:27 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter a Number"<<a;
+	cin>>a;
+	if(a%2==0)
+	cout<<"The given numbers is Even"<<a;
+	else
+	cout<<"The given numbers is Odd"<<a;
+	
+	return 0;
+}
+	
+
